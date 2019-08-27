5a16f0cc24fa1971853f3465ec3de71239366d99
tatiana-gilchrist <tatiana@y2analytics.com>
2019-08-27 11:55:09 -0600

    Third Commit
    Update as_percent function

--- R/as_percent.R
+++ R/as_percent.R
@@ -1,16 +1,16 @@
 #' As Percent
-#' 
+#'
 #' This takes a column of numbers in decimal format and turns it into percent format.
-#' @param df The dataframe with the number column. Defaults to the name frequencies 
+#' @param df The dataframe with the number column. Defaults to the name frequencies
 #' @param var The name of the number column. Defaults to the name result
 #' @return values will go from decimal to percent format like such: 0.03 -> 3%
-#' @examples 
-#' frequencies <- c(.30,.47,.25, .65, .42,.57,.74,.19,.54,.50,.46,.28,.45) %>% 
-#' as.data.frame() %>% 
+#' @examples
+#' frequencies <- c(.30,.47,.25, .65, .42,.57,.74,.19,.54,.50,.46,.28,.45) %>%
+#' as.data.frame() %>%
 #'  rename(
 #'    result = "."
 #'  )
-#'  
+#'
 #'  as_percent()
 
 as_percent <- function(
@@ -18,8 +18,8 @@ as_percent <- function(
   var = result
 ){
   var_flag <- dplyr::enquo(var)
-  
-  df %>% 
+
+  df %>%
     mutate(
       result_per = str_c(!!var_flag * 100, '%')
     )
