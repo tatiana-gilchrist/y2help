8bfb389f6f73e8d59ac56e486f0ac37a672f58c8
tatiana-gilchrist <tatiana@y2analytics.com>
2019-08-27 11:51:48 -0600

    Update as_percent.R

--- R/as_percent.R
+++ R/as_percent.R
@@ -3,7 +3,7 @@
 #' This takes a column of numbers in decimal format and turns it into percent format.
 #' @param df The dataframe with the number column. Defaults to the name frequencies 
 #' @param var The name of the number column. Defaults to the name result
-#' @return values will go from decimal to percent format like such: 0.03 -> 3%
+#' @return values will go from decimal to percent format like such: 0.03 -> "3%"
 #' @examples 
 #' frequencies <- c(.30,.47,.25, .65, .42,.57,.74,.19,.54,.50,.46,.28,.45) %>% 
 #' as.data.frame() %>% 
